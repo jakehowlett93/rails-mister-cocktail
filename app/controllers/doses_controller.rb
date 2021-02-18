86d7dd840435cb5773d967021034ee4890beecb3
jake howlett <jake.howlett93@gmail.com>
2021-02-18 15:32:40 +0000

    create new and create function for doses controller and news view + form for new

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,20 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = params[:cocktail_id]
+    @dose.ingredient_id = params[:ingredient_id]
+    @dose.save
+    redirect_to cocktail_path(params[:cocktail_id])
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient)
+  end
 end
