e8ba54905c85f90136e2df327bbdb327f3fd991d
jake howlett <jake.howlett93@gmail.com>
2021-02-18 14:26:22 +0000

    create the create method and strong params method for it

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,4 +10,16 @@ class CocktailsController < ApplicationController
   def new
     @cocktail = Cocktail.new
   end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
