03f0e6acc828c5f17a9d06f1d19cb282e191e6b4
jake howlett <jake.howlett93@gmail.com>
2021-02-19 16:02:47 +0000

    add lots of styling, remove some buttons and add a back button

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,9 +1,17 @@
 module CocktailsHelper
-  def set_photo_for(instance)
+  def photo_for(instance)
     if instance.photo.attached?
       return cl_image_tag(instance.photo.key, width: 250, height: 250)
     else
       return cl_image_tag('dh51wnb9fkvfzmglfnjx', width: 250, height: 250)
+    end
+  end
+
+  def big_photo_for(instance)
+    if instance.photo.attached?
+      return cl_image_tag(instance.photo.key, width: 300, height: 350)
+    else
+      return cl_image_tag('dh51wnb9fkvfzmglfnjx', width: 300, height: 350)
     end
   end
 end
