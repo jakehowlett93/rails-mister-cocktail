9bdbe11d7c19a5bcc3d42efef0b441e0e6f2121d
jake howlett <jake.howlett93@gmail.com>
2021-02-19 13:25:24 +0000

    create cocktail helper to set image

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def set_photo_for(instance)
+    if instance.photo.attached?
+      return cl_image_tag(instance.photo.key, width: 250, height: 250)
+    else
+      return cl_image_tag('dh51wnb9fkvfzmglfnjx', width: 250, height: 250)
+    end
+  end
 end
